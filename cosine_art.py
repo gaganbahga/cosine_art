160b03e2c073d140a1cfdaff582b8ee5285d68c1
Gagandeep Singh <gaganbahga@gmail.com>
2025-06-15 19:03:35 -0400

    added svg support

--- cosine_art.py
+++ cosine_art.py
@@ -4,10 +4,63 @@
 # author: Gagandeep Singh, 19 Oct, 2018
 
 from os import path
-import imageio
+import imageio.v2 as imageio
 import argparse
 from tqdm import tqdm
 import numpy as np
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+# --- SVG generation support ---
+def generate_svg(img, row_width, freq_factor, wav_color, bg_color, output_file):
+    """
+    Generate an SVG file with cosine curves modulated by the image.
+    :param img: np.ndarray, 2-D image array
+    :param row_width: int, width of each wave row in pixels
+    :param freq_factor: float, frequency scaling factor for the cosine modulation
+    :param wav_color: (int, int, int), RGB tuple for the waveform color
+    :param bg_color: (int, int, int), RGB tuple for the background color
+    :param output_file: str, path to the output SVG file
+    """
+    img = img / img.max()
+    max_freq = 2 * np.pi * freq_factor
+    img = max_freq * img
+    height = row_width * img.shape[0]
+    width = img.shape[1]
+
+    svg = ET.Element('svg', xmlns="http://www.w3.org/2000/svg", width=str(width), height=str(height), version="1.1")
+    # Background
+    bg_hex = '#%02x%02x%02x' % bg_color
+    ET.SubElement(svg, 'rect', x="0", y="0", width=str(width), height=str(height), fill=bg_hex)
+
+    wav_hex = '#%02x%02x%02x' % wav_color
+    for i in tqdm(range(img.shape[0]), desc='Generating SVG rows'):
+        row = img[i]
+        phase = 0
+        points = []
+        for j in range(len(row)):
+            f = row[j]
+            if j == 0:
+                f_prev = f
+            else:
+                f_prev = row[j - 1]
+            block, phase = get_column(f, f_prev, phase, row_width)
+            # Find the y position of the wave (where block==1)
+            y_indices = np.where(block == 1)[0]
+            if len(y_indices) > 0:
+                y = y_indices[0] + i * row_width
+            else:
+                y = (row_width // 2) + i * row_width
+            points.append(f"{j},{y}")
+        # Draw polyline for this row
+        ET.SubElement(svg, 'polyline', points=' '.join(points), stroke=wav_hex, fill="none", stroke_width="1")
+
+    # Pretty print and write
+    rough_string = ET.tostring(svg, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    pretty_svg = reparsed.toprettyxml(indent="  ")
+    with open(output_file, 'w') as f:
+        f.write(pretty_svg)
 
 def main(args):
     img = np.asarray(imageio.imread(args.input))
@@ -23,21 +76,26 @@ def main(args):
     freq_factor = args.freq_factor * 0.3 + 0.1 # just scaling
 
     img = subsample(img, args.num_rows)
-    img = get_cosine_img(img, row_width, freq_factor)
     wav_color = get_color(args.wav_color)
     bg_color = get_color(args.background_color)
 
-    final_img = np.zeros((img.shape[0], img.shape[1], 3), dtype='uint8')
-
-    for i in range(3):
-        final_img[:,:,i][img == 0] = bg_color[i]
-        final_img[:,:,i][img == 1] = wav_color[i]
-
     output_file = args.output_file
     if output_file is None:
-        output_file = path.join(path.dirname(args.input), 'generated.png')
-    imageio.imwrite(output_file, final_img.astype('uint8'))
-    print('Generated image is present at', output_file)
+        output_file = path.join(path.dirname(args.input), 'generated.svg' if getattr(args, 'svg', False) else 'generated.png')
+
+    if args.svg:
+        generate_svg(img, row_width, freq_factor, wav_color, bg_color, output_file)
+        print('Generated SVG image is present at', output_file)
+    else:
+        img = get_cosine_img(img, row_width, freq_factor)
+        final_img = np.zeros((img.shape[0], img.shape[1], 3), dtype='uint8')
+
+        for i in range(3):
+            final_img[:,:,i][img == 0] = bg_color[i]
+            final_img[:,:,i][img == 1] = wav_color[i]
+
+        imageio.imwrite(output_file, final_img.astype('uint8'))
+        print('Generated image is present at', output_file)
 
 def get_color(color):
     """
@@ -171,5 +229,7 @@ if __name__ == '__main__':
                         help='frequency factor in range [0., 1.] for wavforms, higher the number, more compact the wavs')
     parser.add_argument('--invert', action='store_true',
                          help='invert colors, so that dark goes to less frequency and vice-versa')
+    parser.add_argument('--svg', action='store_true', default=False,
+                        help='output SVG instead of PNG')
     args = parser.parse_args()
     main(args)
